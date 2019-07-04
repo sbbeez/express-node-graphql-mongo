e5bc5e70e51d9e829b42751f2c6149083f0caa3b
senthilbalajiganesan <senthil.g.balaji@gmail.com>
2019-07-04 23:01:08 +0530

    ready for deplOyment

--- client/src/service/schema.js
+++ client/src/service/schema.js
@@ -1,4 +1,4 @@
 
 export const getPlayersSchema = `{ getPlayers { _id name thumbnail_url } }`;
 
-export const getPlayerStatsSchema = (ids) => `{ getPlayers(playerList: ["${ids.join('","')}"]) { _id name age } }`;
+export const getPlayerStatsSchema = (ids) => `{ getPlayers(playerList: ["${ids.join('","')}"]) { name age innings_played bowled  matches_won } }`;
--- client/src/service/url.js
+++ client/src/service/url.js
@@ -1,3 +1,4 @@
-const ROOT_URL = "http://localhost:3503/";
+// const ROOT_URL = "http://localhost:3503/";
+const ROOT_URL = "http://3.17.135.197:3503/";
 
 export const graphQl = `${ROOT_URL}graphql`;
